ee1e29176c645b2ce9894223c6b1fbd0b042c91a
FabioSebs <sebsphotosofficial@gmail.com>
2021-12-21 02:39:38 -0500

    Update and Delete

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -5,6 +5,7 @@ import Navbar from './Navbar'
 
 const Create = () => {
     let [productID, setProductID] = useState(undefined)
+    let [reviewID, setReviewID] = useState()
     let [asin, setAsin] = useState([])
     let [name, setName] = useState([])
     let [ratings, setRating] = useState(0)
@@ -17,11 +18,10 @@ const Create = () => {
             if (table === "reviews") {
                 const res = await axios.get(`http://localhost:5000/api/lastrecord/reviews`)
                 return res.data.review_id
-            } else if (table === "products") {
+            } else {
                 const res = await axios.get(`http://localhost:5000/api/lastrecord/products`)
                 return res.data.product_id
             }
-            return undefined
         }
         catch (e) {
             console.log(e)
@@ -65,6 +65,47 @@ const Create = () => {
 
     }
 
+    const updateProduct = async () => {
+        try {
+            const res = await axios.put(`http://localhost:5000/api/update/product/${productID}`, {
+                name: name
+            })
+            setCreated(true)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const updateReview = async () => {
+        try {
+            const res = await axios.put(`http://localhost:5000/api/update/review/${reviewID}`, {
+                rating: ratings,
+                review: review
+            })
+            setCreated(true)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const deleteProduct = async () => {
+        try {
+            const res = await axios.delete(`http://localhost:5000/api/delete/review/${productID}`)
+            console.log(res.status)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const deleteReview = async () => {
+        try {
+            const res = await axios.delete(`http://localhost:5000/api/delete/review/${reviewID}`)
+            console.log(res.status)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
     return (
         <>
             <Navbar />
@@ -89,6 +130,40 @@ const Create = () => {
                     <button type="button" onClick={createReview}>Create</button>
                 </form>
                 <h3 className='success'> {created ? "Successfully Created!" : undefined} </h3>
+
+                <h1 className='updateProd'> Update a Product !</h1>
+                <form className='createForm'>
+                    <label> Product ID </label>
+                    <input type="text" onChange={(e) => { setProductID(parseInt(e.currentTarget.value)) }} />
+                    <label> Product Name </label>
+                    <input type="text" onChange={(e) => { setName(e.currentTarget.value) }} />
+                    <button type="button" onClick={updateProduct}>Update</button>
+                </form>
+
+                <h1 className='updateReview'> Update a Review !</h1>
+                <form className='createForm'>
+                    <label> Review ID </label>
+                    <input type="text" onChange={(e) => { setReviewID(parseInt(e.currentTarget.value)) }} />
+                    <label> Rating </label>
+                    <input type="text" onChange={(e) => { setRating(parseInt(e.currentTarget.value)) }} />
+                    <label> Review </label>
+                    <input type="text" onChange={(e) => { setReview(e.currentTarget.value) }} />
+                    <button type="button" onClick={updateReview}>Update</button>
+                </form>
+
+                <h1 className='deleteProduct'> Delete a Product !</h1>
+                <form className='createForm'>
+                    <label> Product ID </label>
+                    <input type="text" onChange={(e) => { setProductID(parseInt(e.currentTarget.value)) }} />
+                    <button type="button" onClick={deleteProduct}>Delete</button>
+                </form>
+
+                <h1 className='deleteReview'> Delete a Review !</h1>
+                <form className='createForm'>
+                    <label> Review ID </label>
+                    <input type="text" onChange={(e) => { setReviewID(parseInt(e.currentTarget.value)) }} />
+                    <button type="button" onClick={deleteReview}>Delete</button>
+                </form>
             </div>
         </>
     )
